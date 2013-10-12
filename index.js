e02f9c424e2f8aaacde8b82d2ef12006b39d4dfd
Sam Selvanathan <santhonyselvanat@paypal.com>
2013-10-11 17:57:37 -0700

    added model to serve results based on the different request

--- index.js
+++ index.js
@@ -1,22 +1,50 @@
 var express = require("express"),
 	config = require("./config"),
+	mongoClient = require('mongodb').MongoClient,
+	model = require('./model'),	
 	app = express();
 
 
-app.use(function(req, res, next) {
-	console.log(req.headers);
-	if(req.headers.appid && req.headers.appid === config.appid) {
-		next();
-	}
-	else {
-		res.json(401, {
-			error: "Unauthorized api access!"
-		});
-	}
-});
+mongoClient.connect(config.db.url + config.db.name, function(err, db) {
+    if(err) throw err;
 
-app.get("/", function(req, res) {
-	res.json(config);
-});
+    model.init(db);
 
-app.listen(config.port);
+    // middleware for security
+	app.use(function(req, res, next) {
+		if(req.headers.appid && req.headers.appid === config.appid) {
+			next();
+		}
+		else {
+			res.json(401, {
+				error: "unauthorized api access buddy.."
+			});
+		}
+	});
+
+
+	app.get("/", function(req, res) {
+		if(err) throw err;
+
+		var response = function(results) {
+			res.json(results);
+		};
+
+		if(req.params.lat && req.params.long) {
+			model.listByLocation(req.params.lat, response);
+		}
+		else if(req.params.search) {
+			model.search(req.params.lat, response);
+		}
+		else {
+			model.default(response);
+		}
+
+	});
+
+
+	app.listen(config.port);	 
+
+  });
+
+//db.close();
---
+++ model.js
@@ -0,0 +1,22 @@
+var collection;
+
+module.exports = {
+
+	init: function(db) {
+		collection = db.collection("business");
+	},
+
+	listByLocation: function(lat, long, callback) {
+
+	},
+
+	search: function(keyword, callback) {
+
+	},
+
+	default: function(callback) {
+		collection.find().sort({"Business Name":-1}).limit(10).toArray(function(err, results) {
+			callback(results);
+		}); 
+	}
+};
